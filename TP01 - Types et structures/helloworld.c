fffe42e371a0b7d4b908fff244f0b25094bfb037
Christophe CRUZ <31595200+ChristopheCruz@users.noreply.github.com>
2017-09-25 19:09:24 +0200

    Minor updates

--- TP01 - Types et structures/helloworld.c
+++ TP01 - Types et structures/helloworld.c
@@ -1,10 +1,14 @@
 #include <stdio.h>  //pour la fonciton println
 #include <stdlib.h> //pour la macro EXIT_SUCCESS
 
+// Programme de test pour la compilation d'un programme C
+// utiliser gcc dans la console sous linux ou bash MinGw
+// "gcc helloworld.c -o helloworld"
+
 int main(int argc, char ** argv) //point d'entrée du programme C
 {
     printf("Hello Wolrd! \n");
-    return EXIT_SUCCESS; //https://www.gnu.org/software/libc/manual/html_node/Exit-Status.html
+    return EXIT_SUCCESS;         //https://www.gnu.org/software/libc/manual/html_node/Exit-Status.html
 }
 
 
