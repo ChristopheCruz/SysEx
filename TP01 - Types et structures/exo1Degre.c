98280ffc78d0f9664369832e7360fe9014d65290
Christophe CRUZ <31595200+ChristopheCruz@users.noreply.github.com>
2017-09-25 22:28:35 +0200

    BatNav v0.1

---
+++ TP01 - Types et structures/exo1Degre.c
@@ -0,0 +1,24 @@
+#include <stdio.h>  
+#include <stdlib.h> 
+
+float c; // degré Celsius - variable globale
+
+//programme de calcul de conversion des °F en °c
+int main(int argc, char ** argv) //point d'entrée du programme C
+{
+    int f; //degré Fahrenheit - variable locale
+
+    for(f=0; f<300; f+=30)
+    {
+        c = (5.0/9)*(f-32);
+        printf("%f C <=> %d F\n", c, f);
+    }
+
+    //complément sur les segments de mémoire
+    printf("Segment de données: %p\n", &c); //%p affiche formaté d'une adresse, & opérateur d'adressage donnant l'adresse d'une variable
+    printf("Segment de pile: %p\n", &f); //%p affiche formaté d'une adresse, & opérateur d'adressage donnant l'adresse d'une variable
+
+    return EXIT_SUCCESS; 
+}
+
+
--- TP01 - Types et structures/exo2BatNav.c
+++ TP01 - Types et structures/exo2BatNav.c
@@ -1,8 +1,48 @@
 #include <stdio.h>  
 #include <stdlib.h> 
  
+#define _SIZE_ 10
+#define _MER_ '~'
+
+// déclaration des matrices de jeux, bâteaux et tirs pour les deux joureurs 
+char J1Bateaux[_SIZE_][_SIZE_];
+char J2Bateaux[_SIZE_][_SIZE_];
+char J1Tirs[_SIZE_][_SIZE_];
+char J2Tirs[_SIZE_][_SIZE_];
+
+//initialisation d'un matrice de jeu sans bâteaux
+void initMatrice(char tab[_SIZE_][_SIZE_])
+{
+    int i,j;
+    for(i=0; i<_SIZE_; i++)
+    {
+        for(j=0; j<_SIZE_; j++)
+        {
+            tab[i][j] = _MER_;
+        }
+    }
+}
+
+//affichage d'un matrice de jeu
+void afficheMatrice(char tab[_SIZE_][_SIZE_])
+{
+    int i,j;
+    for(i=0; i<_SIZE_; i++)
+    {
+        for(j=0; j<_SIZE_; j++)
+        {
+            putchar(tab[i][j]);
+            putchar(' ');
+        }
+        putchar('\n'); // attention 'a' pour un cactère et "aaa" pour une chaîne de caractères 
+    }
+}
+
+//Programme principal de la bâtaille navale
+
 int main(int argc, char ** argv)
 {
-
+    initMatrice(J1Bateaux);
+    afficheMatrice(J1Bateaux);
     return EXIT_SUCCESS; 
 }
